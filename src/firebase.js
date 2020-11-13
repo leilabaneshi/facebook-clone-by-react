a7d31639cfb8e02b7cdcff491ea1c57e2975d5f9
Leila Baneshi <baneshi.leila@gmail.com>
2020-11-13 02:39:35 +0100

    make HEADER component using Material ui

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCPDy2SKMLhUx56rBNoNxvwApQBwGQgEAw",
+    authDomain: "facebook-clone-ba070.firebaseapp.com",
+    databaseURL: "https://facebook-clone-ba070.firebaseio.com",
+    projectId: "facebook-clone-ba070",
+    storageBucket: "facebook-clone-ba070.appspot.com",
+    messagingSenderId: "900700737204",
+    appId: "1:900700737204:web:be054183e751e79e7fd1e1",
+    measurementId: "G-3R0JM8MVF5"
+};
